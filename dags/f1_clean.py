5b5af0e5ad62bdcd0eecd471a07cfcffc9734cc6
JAguilar <alejandroaguilarc95@gmail.com>
2025-09-21 18:48:17 +0200

    working on env errors

--- dags/f1_clean.py
+++ dags/f1_clean.py
@@ -1,14 +1,16 @@
 # from airflow.decorators import dag, task
 # from airflow.operators.python import PythonOperator
 import pandas as pd
-from datetime import datetime
 from minio import Minio
 from io import BytesIO
-import numpy as np
-from sqlalchemy import create_engine
+from include.utils import  f1_data_dict,sql_f1_schema,sqlengine
 # import logging
 
 # logger = logging.getLogger(__name__)
+
+
+# MinIO connection details
+bucket_name = 'datalake'
 
 # change localhost to minio if running inside docker
 minio_client = Minio(
@@ -18,46 +20,20 @@ minio_client = Minio(
     secure=False
 )
 
-# PostgreSQL connection details
-db_user = 'airflow'
-db_password = 'airflow'
-db_host = 'localhost'
-db_port = '5432'
-db_name = 'airflow'
-f1_schema = 'f1_data'
 
-bucket_name = 'datalake'
-
-
-prefix = 'bronce/session_result/'
-
-dataframes = {}
-
-try:
-    # List all objects in the bucket with the specified prefix
-    print(f"Connecting to MinIO and listing objects in bucket '{bucket_name}' with prefix '{prefix}'")
+for key, value in f1_data_dict.items():
+    prefix = f'bronce/{value}/'
+    print(f"Processing : {prefix}")
+    print(repr(prefix))
     objects = minio_client.list_objects(bucket_name, prefix=prefix, recursive=True)
     for obj in objects:
+        print(obj.object_name)
         if obj.object_name.endswith('.csv'):
-            # Get the object from MinIO
             response = minio_client.get_object(bucket_name, obj.object_name)
-
-            # Read the CSV data from the response stream into a DataFrame
             df = pd.read_csv(BytesIO(response.read()))
-
-
-except Exception as err:
-    print(f"An error occurred: {err}")
-finally:
-    if 'response' in locals():
-        response.close()
-        response.release_conn()
-
-
-engine = create_engine(f'postgresql+psycopg2://{db_user}:{db_password}@{db_host}:{db_port}/{db_name}')  
-
-# Write df_clean to PostgreSQL table 'session_results_clean'
-df.to_sql('session_results', engine,schema = f1_schema  ,if_exists='replace', index=False)
-
-
-engine.dispose() # Closes all connections in the pool
+            # Write to SQL table named after the key
+            df.to_sql(key, sqlengine, schema=sql_f1_schema, if_exists='replace', index=False)
+            print(f"Data from {value} written to table {key} in schema {sql_f1_schema}")
+response.close()
+response.release_conn()
+    
--- include/utils.py
+++ include/utils.py
@@ -1,11 +1,12 @@
 from minio import Minio
 import pandas as pd
+from sqlalchemy import create_engine
 
 
-f1_minio_dict = {
+f1_data_dict = {
+    "results": "session_result",
     "sessions": "sessions",
     "drivers": "drivers",
-    "results": "session_result",
     "starting_grid": "starting_grid",
     "weather": "weather"
 }
@@ -27,4 +28,12 @@ default_args = {
     "retry_delay": pd.Timedelta(minutes=1),
 }
 
+# PostgreSQL connection details
+sql_db_user = 'airflow'
+sql_db_password = 'airflow'
+sql_db_host = 'localhost'
+sql_db_port = '5432'
+sql_db_name = 'airflow'
+sql_f1_schema = 'f1_data'
 
+sqlengine = create_engine(f'postgresql+psycopg2://{sql_db_user}:{sql_db_password}@{sql_db_host}:{sql_db_port}/{sql_db_name}')  
